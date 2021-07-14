939a4327331fc910207705c27a7fee9566521cec
jmcaboy <jmcaboy@naver.com>
2021-07-14 14:28:38 +0900

    2nd

--- GetJSONData.cs
+++ GetJSONData.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs;
+
 
 namespace Hyeon.Function
 {
@@ -22,6 +24,22 @@ namespace Hyeon.Function
             dynamic data = JsonConvert.DeserializeObject(requestBody);//Json으로 만든 Object
             string valueA = data.a;
             
+            BlobServiceClient ClientA = new BlobServiceClient(connStrA);
+            //serviceClient에서 컨테이너 가져오기
+            BlobContainerClient containerA = ClientA.getBlobContainerClient("hyeoncon");
+            //JSON같은 데이터 가져오기
+            BlobClient blobA = containerA.getBlobClient(valueA + ".json");
+            
+            //text로 변환
+            string responseA = "No Data";
+            if(blobA.Exists())
+            {   
+                using (MemoryStream msA = new MemoryStream())
+                {
+                    blobA.DownloadTo(msA);
+                    responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+                }
+            } 
             return valueA;
            
         }
