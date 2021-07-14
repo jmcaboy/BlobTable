ca74f371242b1e2df58fe62cc9644bec95550d35
jmcaboy <jmcaboy@naver.com>
2021-07-14 15:27:22 +0900

    3rd

---
+++ WriteTable.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Storage.Blobs;
+
+
+namespace Hyeon.Function
+{
+    public static class GetJSONData
+    {
+        [FunctionName("GetJSONData")]
+        public static string Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequest req, ILogger log, ExecutionContext context)
+
+        {
+            string connStrA = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string valueA = data.a;
+
+            BlobServiceClient clientA = new BlobServiceClient(connStrA);
+            BlobContainerClient containerA = clientA.GetBlobContainerClient("mingyucon");
+            BlobClient blobA = containerA.GetBlobClient(valueA + ".json");
+
+            string responseA = "No Data";
+
+            if (blobA.Exists())
+            {
+                using (MemoryStream msA = new MemoryStream())
+                {
+                    blobA.DownloadTo(msA);
+                    responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+                }
+            }
+            return responseA;
+
+           
+        }
+    }
+}
